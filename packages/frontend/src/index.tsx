676e9fc691f27cd91173e1168b22f152dadd252e
Alkan <alkanorgun@gmail.com>
2022-07-15 13:51:34 +0300

    use double quote in the app

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
