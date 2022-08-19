0d7a16174af11209239bc8d8fe3091e5e93ac069
Alkan <alkanorgun@gmail.com>
2022-08-19 22:26:40 +0300

    Header :: with counter

--- packages/frontend/src/components/Header.tsx
+++ packages/frontend/src/components/Header.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-
 import "../styles/Header.scss";
+import { useAppSelector } from "../app/hooks";
 
 const Header = (): JSX.Element => {
+  const curStep = useAppSelector((state) => state.sms.curStep);
   return (
-    <header>Header</header>
+    <header>
+      Header. Cur step is: {curStep}
+    </header>
   );
 };
 
