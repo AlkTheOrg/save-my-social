c85429d0b3d387c7e63d7d2f3259493cd068d74d
Alkan <alkanorgun@gmail.com>
2022-11-06 23:12:32 +0300

    redditApiService :: fetchSavedModels

--- packages/frontend/src/features/reddit/redditApiService.ts
+++ packages/frontend/src/features/reddit/redditApiService.ts
@@ -3,3 +3,25 @@ import { REDDIT_BACKEND } from "../../constants/apiEndpoints";
 
 export const fetchAuthURL = (): Promise<{ data: { url: string } }> =>
   axios.get(`${REDDIT_BACKEND}/auth-url`);
+
+//* Corresponds to ProcessedSavedChildren in api
+export type SavedModel = {
+  id: string,
+  kind: "Comment" | "Post",
+  kindID: string,
+  subreddit: string,
+  title: string,
+  over_18: boolean,
+  permalink: string,
+  link: string,
+};
+
+type SavedModelsResponse = {
+  models: SavedModel[],
+  lastQueried: string,
+};
+
+export const fetchSavedModels = (accessToken: string, after = "") =>
+  axios.get<SavedModelsResponse>(
+    `${REDDIT_BACKEND}/savedModels?access_token=${accessToken}&after=${after}`,
+  );
