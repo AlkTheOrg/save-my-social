c1c417794bf6a3e6d27273262a2f47841cda73da
Alkan <alkanorgun@gmail.com>
2022-11-07 22:11:07 +0300

    RedditRoutes :: require access token for savedModels

--- packages/api/src/routes/redditRoutes.ts
+++ packages/api/src/routes/redditRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import redditController from '../controllers/redditController.js';
+import { requireAccessToken } from "../middlewares/index.js";
 
 const router = Router();
 router.get('/auth-url', redditController.redirectUrl);
 router.get('/login', redditController.login);
 router.get('/logged', redditController.logged);
-router.get('/savedModels', redditController.getSavedModels);
+router.get('/savedModels', requireAccessToken, redditController.getSavedModels);
 
 export default router;
