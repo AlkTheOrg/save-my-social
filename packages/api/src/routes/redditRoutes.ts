c18321f2624aead09d00c12c3a618a1feef2aaf8
Alkan <alkanorgun@gmail.com>
2022-11-17 01:07:20 +0300

    mw :: todo

--- packages/api/src/routes/redditRoutes.ts
+++ packages/api/src/routes/redditRoutes.ts
@@ -6,6 +6,7 @@ const router = Router();
 router.get('/auth-url', redditController.redirectUrl);
 router.get('/login', redditController.login);
 router.get('/logged', redditController.logged);
+// TODO makes this post and take params from body
 router.get('/savedModels', requireAccessToken, redditController.getSavedModels);
 
 export default router;
