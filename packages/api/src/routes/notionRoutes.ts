31af3cdbc24951c13cf450f0a9bf0282cdc9e5f6
Alkan <alkanorgun@gmail.com>
2022-11-18 23:37:43 +0300

    NotionRouters :: comment to be used function

--- packages/api/src/routes/notionRoutes.ts
+++ packages/api/src/routes/notionRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import notionController from "../controllers/notionController.js";
-import { requireAccessToken } from "../middlewares/index.js";
+// import { requireAccessToken } from "../middlewares/index.js";
 
 const router = Router();
 router.get('/login', notionController.login);
