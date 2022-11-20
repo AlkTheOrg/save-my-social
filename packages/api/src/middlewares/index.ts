9dfbc77ff3050bff6321dd06e60bfcadc525e7f0
Alkan <alkanorgun@gmail.com>
2022-11-20 18:14:03 +0300

    mw :: got rid of requireAccessToken

--- packages/api/src/middlewares/index.ts
+++ packages/api/src/middlewares/index.ts
@@ -1,16 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { sendMsgResponse } from '../lib/index.js';
 
-export const requireAccessToken = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-): void => {
-  if (!req.query.accessToken)
-    res.status(404).send({ msg: 'Access token must be provided.' });
-  else next();
-};
-
 export const requireAccessTokenInBody = (
   req: Request,
   res: Response,
