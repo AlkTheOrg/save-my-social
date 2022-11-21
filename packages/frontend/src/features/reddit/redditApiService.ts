eefe9da258ad90486cfdcbb108e413152744c6e8
Alkan <alkanorgun@gmail.com>
2022-11-22 00:14:15 +0300

    RedditApiService :: lint fix

--- packages/frontend/src/features/reddit/redditApiService.ts
+++ packages/frontend/src/features/reddit/redditApiService.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
 import { REDDIT_BACKEND } from "../../constants/apiEndpoints";
 
-export const fetchAuthURL = () => axios.get<{ url: string }>(`${REDDIT_BACKEND}/auth-url`);
+export const fetchAuthURL = () =>
+  axios.get<{ url: string }>(`${REDDIT_BACKEND}/auth-url`);
