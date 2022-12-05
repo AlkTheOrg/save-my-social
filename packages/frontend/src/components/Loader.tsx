912ec966c941c005431287300c2df3a8d978598e
Alkan <alkanorgun@gmail.com>
2022-12-06 00:00:07 +0300

    Loader component

---
+++ packages/frontend/src/components/Loader.tsx
@@ -0,0 +1,22 @@
+import "../styles/Loader.scss";
+// credit to https://loading.io/css/
+const Loader = () => {
+  return (
+    <div className="lds-default">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loader;
