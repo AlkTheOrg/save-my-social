65bc15414e336155daa14bdf1d4d3229dda06303
Alkan <alkanorgun@gmail.com>
2023-01-25 00:44:44 +0300

    TabTrap :: refactor

--- packages/frontend/src/components/TabTrap.tsx
+++ packages/frontend/src/components/TabTrap.tsx
@@ -5,6 +5,12 @@ import {
 // eslint-disable-next-line max-len
 const focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex]:not([tabindex="-1"]), [contenteditable]';
 
+const getFocusableElements = (target: HTMLElement | null) => Array.from(
+  target?.querySelectorAll(focusableElementsString) || [],
+) as HTMLElement[];
+const getFirstFocusableElement = (target: HTMLElement | null) =>
+  target?.querySelector(focusableElementsString);
+
 type Props = {
   children: ReactNode;
 };
@@ -13,15 +19,11 @@ const TabTrap: FC<Props> = ({ children }) => {
   const [prevFocusedElem, setPrevFocusedElem] = useState<HTMLElement | null>(null);
   const trapWrapperRef = useRef<HTMLDivElement>(null);
 
-  const getFocusableElements = () => Array.from(
-    trapWrapperRef.current?.querySelectorAll(focusableElementsString) || [],
-  ) as HTMLElement[];
-
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
       if (e.key === "Tab") {
         //* content may be dynamic, so we get all focusable elements every time
-        const focusableElements = getFocusableElements();
+        const focusableElements = getFocusableElements(trapWrapperRef.current);
         const firstElement = focusableElements[0];
         const lastElement = focusableElements[focusableElements.length - 1];
         if (e.shiftKey) {
@@ -47,11 +49,8 @@ const TabTrap: FC<Props> = ({ children }) => {
   }, [prevFocusedElem]);
 
   useEffect(() => {
-    const focusableElements = getFocusableElements();
-    if (focusableElements.length > 0) {
-      const firstElement = focusableElements[0] as HTMLElement;
-      firstElement.focus();
-    }
+    const firstFocusableElement = getFirstFocusableElement(trapWrapperRef.current);
+    (firstFocusableElement as HTMLElement)?.focus();
   }, []);
 
   return <div ref={trapWrapperRef} tabIndex={-1}>{children}</div>;
