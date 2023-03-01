5948fbbfffe59faea6157cb60fbea232ef935f53
Alkan <alkanorgun@gmail.com>
2023-03-01 09:40:37 +0300

    frontend(test): WindowOpener init tests

---
+++ packages/frontend/src/__tests__/components/WindowOpener.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable max-len */
+import { render } from "@testing-library/react";
+import WindowOpener, { WindowOpenerProps } from "../../components/WindowOpener";
+
+describe("WindowOpener", () => {
+  it("should open a window", () => {
+    const onOpen = jest.fn();
+    const { container } = render(
+      <WindowOpener
+        shouldBeOpened
+        target="http://localhost:3005/test"
+        onOpen={onOpen}
+      />,
+    );
+    expect(onOpen).toHaveBeenCalled();
+    expect(global.open).toHaveBeenCalled();
+    expect(container).toBeInTheDocument();
+  });
+
+  const defaultProps: WindowOpenerProps = {
+    target: "https://www.example.com",
+    shouldBeOpened: false,
+    width: 100,
+    height: 100,
+    marginTop: 5,
+    marginLeft: 5,
+  };
+
+  it("should open a new window with the specified dimensions and URL when shouldBeOpened is true", () => {
+    const { container } = render(<WindowOpener {...defaultProps} shouldBeOpened />);
+    expect(container.querySelector("div")).toHaveStyle("display: none");
+    expect(global.open).toHaveBeenCalledWith(
+      defaultProps.target,
+      "_blank",
+      `width=${defaultProps.width},height=${defaultProps.height},top=${defaultProps.marginTop},left=${defaultProps.marginLeft}`,
+    );
+  });
+
+  it("should call onMessage when a message is received", () => {
+    const onMessage = jest.fn();
+    render(<WindowOpener {...defaultProps} onMessage={onMessage} />);
+    global.dispatchEvent(new MessageEvent("message"));
+    expect(onMessage).toHaveBeenCalled();
+  });
+});
